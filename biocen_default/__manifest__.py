45a45344aee5f2f9cc10954382f91ac6370abdec
leobozzi <lbozzi@gmail.com>
2023-08-05 10:43:30 -0300

    update

--- biocen_default/__manifest__.py
+++ biocen_default/__manifest__.py
@@ -221,7 +221,7 @@
         'https://github.com/leobozzi/cl-biocen cl-biocen',
         #'https://github.com/leobozzi/odoo-addons-utils.git',
         'https://github.com/odoo/enterprise enterprise',
-        #'https://github.com/vangrow/account-modules.git',
+        'https://github.com/vangrow/account-modules.git',
 
 
         # Odoomates
