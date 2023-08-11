3c2a49e4763714e1c8dc13e6a53fba1228ff98b3
leobozzi <lbozzi@gmail.com>
2023-08-11 15:41:03 -0300

    update

--- biocen_default/models/account_move.py
+++ biocen_default/models/account_move.py
@@ -3,50 +3,56 @@
 
 from odoo import api, fields, models
 from odoo.tools.translate import _
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError, UserError
 
+import datetime
 
 class AccountMove(models.Model):
     _inherit = "account.move"
     _description = 'account.move'
 
-    usd_currency_rate = fields.Float(
-        string="USD Currency rate",
-        compute="_computed_usd_currency_rate"
+    def get_usd_currency_rate(self):
+        rate_ids = self.env['res.currency.rate'].search([('currency_id.name', '=', 'USD'), ('name', '<=', datetime.datetime.now().strftime('%Y-%m-%d'))], limit=1)
+        if rate_ids:
+            return rate_ids[0].inverse_company_rate
+        return 0.0
+    
+    def get_usd_currency_id(self):
+        return self.env['res.currency'].search([('name','=','USD')])
+        
+    usd_currency_rate_agree = fields.Float(
+        string="USD Currency rate agree",
+        default=get_usd_currency_rate
     )
 
-    usd_total = fields.Monetary(
-        string="USD Total",
-        compute="_computed_usd_total"
+    usd_currency_id = fields.Many2one(
+        comodel_name='res.currency',
+        default=get_usd_currency_id
     )
 
-    usd_residual = fields.Monetary(
-        string="USD Residual",
-        compute="_computed_usd_residual"
+    usd_total_agree = fields.Monetary(
+        string="USD Total agree",
+        compute="_computed_usd_total_agree"
     )
 
-    @api.depends('invoice_date')
-    def _computed_usd_currency_rate(self):
-        for res in self:
-            rate_ids = self.env['res.currency.rate'].search(
-                [('currency_id.name', '=', 'USD'), ('name', '<=', res.invoice_date)], limit=1)
-            if rate_ids:
-                res.usd_currency_rate = rate_ids[0].inverse_company_rate
-            else:
-                res.usd_currency_rate = 0.0
+    usd_residual_agree = fields.Monetary(
+        string="USD Residual agree",
+        compute="_computed_usd_residual_agree"
+    )
 
-    @api.depends('usd_currency_rate','amount_total')
-    def _computed_usd_total(self):
-        for res in self:
-            if res.usd_currency_rate != 0:
-                res.usd_total = res.amount_total / res.usd_currency_rate
+    @api.depends('usd_currency_rate_agree', 'amount_total')
+    def _computed_usd_total_agree(self):
+        for rec in self:
+            if rec.currency_id == rec.usd_currency_id:
+                rec.usd_total_agree = rec.amount_total
             else:
-                res.usd_total = 0.0
+                rec.usd_total_agree = rec.amount_total / rec.usd_currency_rate_agree if rec.usd_currency_rate_agree != 0 else 0.0
 
-    @api.depends('usd_currency_rate','amount_residual')
-    def _computed_usd_residual(self):
-        for res in self:
-            if res.usd_currency_rate != 0:
-                res.usd_residual = res.amount_residual / res.usd_currency_rate
+    @api.depends('usd_currency_rate_agree', 'amount_residual')
+    def _computed_usd_residual_agree(self):
+        for rec in self:
+            if rec.currency_id == rec.usd_currency_id:
+                rec.usd_residual_agree = rec.amount_residual
             else:
-                res.usd_residual = 0.0
+                rec.usd_residual_agree = rec.amount_residual / rec.usd_currency_rate_agree if rec.usd_currency_rate_agree != 0 else 0.0
+            
